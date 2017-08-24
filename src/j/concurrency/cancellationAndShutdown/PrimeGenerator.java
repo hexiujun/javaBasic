4e727200a687c84dac40e065d8d304374e15f114
hexiujun <hxj1043@sina.com>
2017-08-24 17:11:07 +0800

    update

--- src/j/concurrency/cancellationAndShutdown/PrimeGenerator.java
+++ src/j/concurrency/cancellationAndShutdown/PrimeGenerator.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * Created by j on 2017/8/15.
  * :ThreadSafe
+ * 一种协作式的取消任务的机制：设置某个“已请求取消”的标志，而任务定期的查看该标志。
  */
 
 public class PrimeGenerator implements Runnable {
@@ -24,9 +25,11 @@ public class PrimeGenerator implements Runnable {
         }
     }
 
-    public void cancel() { cancelled = true; }
+    public void cancel() {
+        cancelled = true;
+    }
 
     public synchronized List<BigInteger> get() {
-        return new ArrayList<BigInteger>(primes);
+        return new ArrayList<>(primes);
     }
 }
